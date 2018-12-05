f32054431bb1b9d7e950d30521c0ae535ed42655
Daniel Worley <danworley@gmail.com>
2018-12-05 12:19:01 -0500

    Empty app scaffold

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name searchVizApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the searchVizApp
+ */
+angular.module('searchVizApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
